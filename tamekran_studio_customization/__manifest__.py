8ee37ef24af2a4dbf8b9d33421906ea1d7e35eb1
Mostafa Bagheri <baghery00@gmail.com>
2024-11-19 11:27:44 +0300

    tamekran_studio_customization

--- tamekran_studio_customization/__manifest__.py
+++ tamekran_studio_customization/__manifest__.py
@@ -24,20 +24,26 @@ It contains the apps created with Studio and the customizations of existing apps
         'purchase',
         'stock',
         'website_sale',
+        'uom'
     ],
     'data': [
+        'data/ir_actions_server.xml',
+        # 'data/base_automation.xml',
         'data/res_groups.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_ui_menu.xml',
         'data/ir_ui_view_final.xml',
+        'data/ir_actions_report.xml',
+        
         'data/ir_model.xml',
         'data/ir_model_fields.xml',
-        'data/ir_ui_view.xml',
-        'data/ir_actions_act_window.xml',
-        'data/ir_actions_report.xml',
-        'data/ir_actions_server.xml',
-        'data/ir_ui_menu.xml',
-        'data/ir_filters.xml',
-        'data/base_automation.xml',
         'data/ir_model_access.xml',
+        # 'data/ir_ui_view.xml',
+        # 'data/ir_actions_act_window2.xml',
+        
+        
+        'data/ir_filters.xml',
+        
     ],
     'application': False,
     'license': 'OPL-1',
